53373222265ec5842ea7ff05bf44acceb7cc8298
Ryan H <Ryan@Ryans-MacBook-Pro-2.local>
2024-04-25 19:12:03 +0700

    add schemas for ordered_items and menu_items

---
+++ db/schema/02_orders.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    time_ordered TIMESTAMP,
+    total_cost SMALLINT,
+    completed_at TIMESTAMP
+);
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id, phone, email, password, is_owner) VALUES
+  (1, '1234567890', 'user1@example.com', 'password1', false),
+  (2, '9876543210', 'user2@example.com', 'password2', false),
