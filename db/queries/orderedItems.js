711d495edcee4b0a57c8fe05683d98fde73b75d6
brettg17 <brettgelinas7@gmail.com>
2024-04-26 17:56:57 -0600

    update helper function and fetch all ordered items

--- db/queries/orderedItems.js
+++ db/queries/orderedItems.js
@@ -1,2 +1,15 @@
 const db = require('../connection');
 
+// fetch all ordered items
+const allOrderedItems = () => {
+  return db.query('SELECT * FROM ordered_items;')
+    .then(data => {
+      return data.rows;
+    });
+};
+
+
+module.exports = {
+  allOrderedItems,
+
+}
