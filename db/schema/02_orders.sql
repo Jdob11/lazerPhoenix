6f1d6c51c746e658a2dc2ff401e3fcb49be07b2f
Ryan H <Ryan@Ryans-MacBook-Pro-2.local>
2024-04-26 18:42:24 +0700

    add seed data for menu items

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -1,7 +1,5 @@
-DROP TABLE IF EXISTS orders CASCADE;
-
 CREATE TABLE orders (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
     time_ordered TIMESTAMP,
     total_cost SMALLINT,
--- db/seeds/03_menu_item_seeds.sql
+++ db/seeds/03_menu_item_seeds.sql
@@ -1,5 +1,41 @@
 INSERT INTO menu_items (name, description, price, category, image_url) VALUES
-  ('taco', 'hard taco, ground beef, cheese, tomato and lettuce', 800, 'Entree', 'taco.jpg'),
-  ('hotdog', 'bun, hotdog, choice of ketchup, mustard, and or relish', 600, 'Entree', 'hotdog.jpg'),
-  ('pizza', 'dough, tomato, onions', 1600, 'Entree', 'pizza.jpg')
+
+  ('Takoyaki', 
+  'Deep fried wheat balls with octopus.', 
+  900, 
+  'Appetizers', 
+  'https://tokyonoodleshop.com/med/Takoyaki.jpg'),
+  
+  ('Chicken Karage', 
+  '6 pieces of panko coated chicken, 
+  deep fried & served with ginger sauce.', 
+  1300, 
+  'Appetizers', 
+  'https://tokyonoodleshop.com/med/Chicken-Kara-age-scaled.jpg'),
+
+  ('Beef Sashimi',
+  'Thinly sliced aaa ribeye seared and served with spicy ponzu sauce.',
+  1400,
+  'Appetizers',
+  'https://tokyonoodleshop.com/med/Beef-Sashimi.jpg'),
+
+  ('Spicy Chicken Ramen',
+  'A savoury combination of spicy chicken in the house famous chicken broth and house-made ramen noodles, stopped with green onions, corn.',
+  1600,
+  'Entree'
+  'https://tokyonoodleshop.com/med/Spicy-Seafood-Ramen.jpg'),
+
+  ('Deluxe Tokyo Sashimi',
+  '(36pcs)',
+  7500,
+  'Entrees'
+  'https://tokyonoodleshop.com/med/Special-Sashimi.jpg'),
+
+  ('Seafood Fried Rice',
+  'Japanese style seafood fried rice.',
+  1500,
+  'Entrees',
+  'https://tokyonoodleshop.com/med/Seafood-Fried-Rice.jpg'),
+
+ 
   ;
