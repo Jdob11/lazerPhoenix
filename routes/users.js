849ae10ac760bc5f747e516a3f06480dc47cc3ce
brettg17 <brettgelinas7@gmail.com>
2024-04-27 15:04:54 -0600

    route to check if the user is an owner or a customer

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ const express = require('express');
 const db = require('../db/connection');
 const router  = express.Router();
 const { addMenuItem } = require('../db/queries/menuItems')
+const { getUserById } = require('../db/queries/users')
 
 router.get('/', (req, res) => {
   res.render('users');
@@ -17,6 +18,32 @@ router.get('/', (req, res) => {
 // Route to handle form submission and add menu item to the database
 router.post('/addMenuItem', addMenuItem);
 
+
+// Route to fetch user by ID and determine if owner
+router.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+
+  // Fetch user by ID from the database
+  db.query('SELECT * FROM users WHERE id = $1', [userId])
+  .then(queryResult => {
+    const user = queryResult.rows[0];
+    if (!user) {
+      res.status(404).send("No user exists");
+    } else {
+      // Check if user is an owner
+      const isOwner = user.is_owner;
+      if (isOwner) {
+        res.send('Welcome Mr. Owner! Feel free to edit the menu even though it is already perfect!');
+      } else {
+        res.send('Welcome, customer! enjoy our critically acclaimed beautiful hotdogs and beautiful tacos!');
+      }
+    }
+  })
+  .catch(err => {
+    console.error("error fetching user:", err);
+    res.status(500).send("Internal Server Error");
+  });
+});
+
+
 module.exports = router;
-
-
