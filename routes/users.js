37a4ffcee3d51dd20e02be73d8c27c041ae60561
brettg17 <brettgelinas7@gmail.com>
2024-04-28 10:33:06 -0600

    repopulated lost code

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,33 @@ router.get('/menuItems', async (req, res) => {
   }
 });
 
+// Route to fetch user by ID and determine if owner
+router.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+
+  // Fetch user by ID from the database
+  db.query('SELECT * FROM users WHERE id = $1', [userId])
+  .then(queryResult => {
+    const user = queryResult.rows[0];
+    if (!user) {
+      res.status(404).send("No user exists");
+    } else {
+      // Check if user is an owner
+      const isOwner = user.is_owner;
+      if (isOwner) {
+        res.send('Welcome Mr. Owner! Feel free to edit the menu even though it is already perfect!');
+      } else {
+        res.send('Welcome, customer! enjoy our critically acclaimed beautiful hotdogs and beautiful tacos!');
+      }
+    }
+  })
+  .catch(err => {
+    console.error("error fetching user:", err);
+    res.status(500).send("Internal Server Error");
+  });
+});
+
+
 router.post('/addMenuItem', addMenuItem);
 
 router.post('/editMenuItem', editMenuItem);
