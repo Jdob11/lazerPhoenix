6c185b1e1d7228b650005ba9469ce687abed066d
rhuynh17 <rhuynh1106@gmail.com>
2024-05-03 21:56:50 -0600

    added sms twilio func

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -94,19 +94,20 @@ const createOrderElement = (orderData, orderItems) => {
   const $completeOrderButton = $('<button>').addClass('completeOrderButton').text('Complete Order');
 
   $orderContainer.append($orderNumberDiv, $userInfo, $foodAndPriceContainer, $total, $timeEstimate, $completeOrderButton);
+
   $orderContainer.on('click', '.estimateButton', function() {
     console.log(' estimate button clicked');
     const phoneNumber = '+17807295721';
     const estimateMinutes = $('#estimate').val();
-    // sendEstimateSMS(phoneNumber, estimatedMinutes);
-    // console.log("estimate SMS sent to:", phoneNumber);
+    sendEstimateSMS(phoneNumber, estimatedMinutes);
+    console.log("estimate SMS sent to:", phoneNumber);
   });
 
   $orderContainer.on('click', '.completeOrderButton', function() {
     console.log('Complete Order button clicked');
     const phoneNumber = '+17807295721';
-    // sendOrderCompletedSMS(phoneNumber);
-    // console.log("order completed SMS sent to:", phoneNumber);
+    sendOrderCompletedSMS(phoneNumber);
+    console.log("order completed SMS sent to:", phoneNumber);
   });
 
   return $orderContainer;
