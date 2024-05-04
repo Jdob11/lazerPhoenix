114c492f1d2ab8e31a5b0a684ed092f376c1df8a
rhuynh17 <rhuynh1106@gmail.com>
2024-05-03 19:35:37 -0600

    owner button route + twilio integration - not working

---
+++ public/scripts/owner.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+  $('.menuContainer').on('click', '.estimateButton', function() {
+    console.log(' estimate button clicked');
+    const phoneNumber = '+17807295721';
+    const estimateMinutes = $('#estimate').val();
+    // sendEstimateSMS(phoneNumber, estimatedMinutes);
+    // console.log("estimate SMS sent to:", phoneNumber);
+  });
+
+  $('.menuContainer').on('click', '.completeOrderButton', function() {
+    console.log('Complete Order button clicked');
+    const phoneNumber = '+17807295721';
+    // sendOrderCompletedSMS(phoneNumber);
+    // console.log("order completed SMS sent to:", phoneNumber);
+  });
+});
